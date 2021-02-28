3bb0c4e38fadcbd1797d8d94208c1116fe1e04f9
Edwar Andres Pineda Rincon <eapinedar@poligran.edu.co>
2021-02-27 21:59:12 -0800

    Create network  and delete docker sh

---
+++ acme-network/deletedocker.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+###########################################
+#
+# Simple Shell script to clean/remove all container/images
+#
+# The script will 
+#  - first stop all running containers (if any),
+#  - remove containers
+#  - remove images
+#  - remove volumes
+#
+
+# stop all running containers
+echo '####################################################'
+echo 'Stopping running containers (if available)...'
+echo '####################################################'
+docker stop $(docker ps -aq)
+
+# remove all stopped containers
+echo '####################################################'
+echo 'Removing containers ..'
+echo '####################################################'
+docker rm -f $(docker ps -aq)
+
+
+# remove all images
+#echo '####################################################'
+#echo 'Removing images ...'
+#echo '####################################################'
+#docker rmi $(docker images -q)
+
+# remove all stray volumes if any
+echo '####################################################'
+echo 'Revoming docker container volumes (if any)'
+echo '####################################################'
+docker volume rm $(docker volume ls -q)
+
+
+
+echo '####################################################'
+echo 'pruning docker VOLUMES'
+echo '####################################################'
+docker volume prune -f
+
+echo '####################################################'
+echo 'pruning networks'
+echo '####################################################'
+docker network prune -f
+
+echo '####################################################'
+echo 'Removing crypto config'
+echo '####################################################'
+rm -rf crypto-config
