40f95e56fff2c0661b244e1b29882cbeef4f178e
Edwar Andres Pineda Rincon <eapinedar@poligran.edu.co>
2021-02-28 18:42:51 -0800

    Add shells for create and delete netowrk

---
+++ red/create-network.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+###########################################
+#
+
+echo '####################################################'
+echo 'Recreate crypto config'
+echo '####################################################'
+cryptogen generate --config=./crypto-config.yaml
+
+
+echo '####################################################'
+echo 'Creación de bloque génesis'
+echo '####################################################'
+
+configtxgen -profile HistoriaClinicaOrdererGenesis -channelID system-channel -outputBlock ./channel-artifacts/genesis.block
+
+echo '####################################################'
+echo 'Creación Transacción del Canal'
+echo '####################################################'
+configtxgen -profile HistoriaClinicaChannel -outputCreateChannelTx ./channel-artifacts/channel.tx -channelID historiaclinicaelectronica
+
+echo '####################################################'
+echo 'Creación Anchor transaction files'
+echo '####################################################'
+
+configtxgen -profile HistoriaClinicaChannel -outputAnchorPeersUpdate ./channel-artifacts/Eps1MSPanchors.tx -channelID historiaclinicaelectronica -asOrg Eps1MSP
+
+
+echo '####################################################'
+echo 'CHANNEL_NAME=historiaclinicaelectronica'
+echo '####################################################'
+export CHANNEL_NAME=historiaclinicaelectronica
+export VERBOSE=false
+export FABRIC_CFG_PATH=$PWD
+
+echo '####################################################'
+echo 'LEVANTAR RED DOCKER'
+echo '####################################################'
+CHANNEL_NAME=$CHANNEL_NAME docker-compose -f docker-compose-cli-couchdb.yaml up -d
+
+
+echo '####################################################'
+echo 'LEVANTAR PORTAINER'
+echo '####################################################'
+
+docker volume create portainer_data
+
+
+docker run -d -p 8000:8000 -p 9000:9000 -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer
